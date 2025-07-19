a9b3678ce26a82dc15406c3a6f71f491db21805b
Sillius Goober <scriptgoober@protonmail.com>
2025-07-18 23:23:41 -0500

    first ever commitclearclear huzzahclear! commiting all files to the repo

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace CustomNapalm;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    GameObject bouncyCube;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} is loading!!!11!");
+
+        string pluginfolder = System.IO.Path.GetDirectoryName(GetType().Assembly.Location);
+        string bundleName = "napalm_assets_all_d73cb56529cc1aab54f6984b9e0d8ad7.bundle";
+        string prefabName = "Assets/bouncy.prefab";
+
+        bouncyCube = AssetBundle.LoadFromFile($"{pluginfolder}\\{bundleName}").LoadAsset<GameObject>(prefabName);
+        Logger.LogInfo(bouncyCube);
+        Logger.LogInfo($"{pluginfolder}\\{bundleName}");
+        Logger.LogInfo("trying to load assetbundle");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GameObject newNapalm = Instantiate(bouncyCube);
+            newNapalm.transform.position = new Vector3(0, 20, 0);
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CustomNapalm
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "CustomNapalm";
+        public const string PLUGIN_NAME = "Custom Napalm";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
