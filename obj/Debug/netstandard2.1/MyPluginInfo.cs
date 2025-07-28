1dac987648243e6f862248bd3583a99f7aff3210
silli <scriptgoober@protonmail.com>
2025-07-27 21:52:41 -0400

    idk man

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomNapalm
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "CustomNapalm";
-        public const string PLUGIN_NAME = "Custom Napalm";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
